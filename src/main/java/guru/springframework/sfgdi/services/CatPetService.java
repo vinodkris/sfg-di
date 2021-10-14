d79ab85521df0623d0748b2119be2fb01acfa3cb
vinodkris <vinodkris123@gmail.com>
2021-10-14 06:58:16 +0100

    - For DI assignment

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -1,12 +1,15 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
-@Service("cat")
+@Profile("cat")
+@Service("pet")
 public class CatPetService implements PetService {
+
     @Override
     public String getPetType() {
         return "Cats Are the Best!";
